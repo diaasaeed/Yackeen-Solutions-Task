bc6bc26742b025bed5d2a161cccd4b6081938cc1
diaasaeed <diaasaeed12@gmail.com>
2021-12-25 07:33:12 +0200

    custom network function  to get any request GET

--- YSolutionsTask/NetWork/API Request.swift
+++ YSolutionsTask/NetWork/API Request.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+class API: NSObject {
+
+static let sharedInstance = API()
+
+    func getRequest<T: Decodable>(url: String , onCompletion: @escaping (T?,Int, Error?)->()) {
+
+
+        var request = URLRequest(url:URL(string:url)!)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type") // header
+        request.httpMethod = "GET"
+        print("shared URL : \(request)")
+
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            let statusCode =  response as? HTTPURLResponse
+
+            if let error = error {
+                onCompletion(nil, statusCode?.statusCode ?? 0, error)
+                
+            } else {
+                guard let data = data else {  return  }
+
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode == 200 {
+                    do {
+                        let jsonData = try JSONDecoder().decode(T.self, from: data)
+                        onCompletion(jsonData, statusCode?.statusCode ?? 0, nil)
+
+                    } catch let error as NSError {
+                       onCompletion(nil, statusCode?.statusCode ?? 0, error)
+                    }
+                } else {
+                    onCompletion(nil, statusCode?.statusCode ?? 0, error)
+                }
+            }
+        }
+        task.resume()
+    }
+
+
+    private override init() {}
+}
